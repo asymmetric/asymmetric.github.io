264830c1664c9ddbcc1129bbb8fcb064c7e5ead2
Lorenzo Manacorda <lorenzo@mailbox.org>
2022-05-31 11:22:53 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      name = "blagh";
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        inherit name;
+        buildInputs = with pkgs; [
+          ruby_3_1 gcc cmake
+        ];
+      };
+    };
+}
